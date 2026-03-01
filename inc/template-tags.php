b4361d9545ab4ff5a6559bc4700f645b4cd142c1
DHOMD <DHO@dho.im>
2026-03-01 09:57:49 -0500

    Update to 1.23

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -408,7 +408,7 @@ function simclick_entry_author( $date = true, $author = true ) {
 		// Get the author name; wrap it in a link.
 		$byline = printf(
 			/* translators: %s: post author */
-			'<span class="author vcard"><span class="author-label">By </span>%s<a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . get_the_author() . '</a></span>', ''
+			'<span class="author vcard"><span class="author-label">By </span>%s<a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>', ''
 		);
 	}
 }
